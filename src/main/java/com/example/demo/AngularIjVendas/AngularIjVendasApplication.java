e749cff739c2017ee6fc72890111e68b0a83a854
“a-vecchi” <alexandre_v@yahoo.com>
2021-06-30 18:10:29 -0300

    Criando projeto

---
+++ src/main/java/com/example/demo/AngularIjVendas/AngularIjVendasApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.AngularIjVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularIjVendasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularIjVendasApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/demo/AngularIjVendas/model/repository/ServicoRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.AngularIjVendas.model.repository;
+
+import com.example.demo.AngularIjVendas.model.entity.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+}
